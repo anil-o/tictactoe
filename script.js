e5e2a8da14b363c7a096c6a03c400a1d847d08e0
freedomWithFinanceAndCoding <orhan_anil@yahoo.de>
2022-09-19 21:30:58 +0200

    update project

--- script.js
+++ script.js
@@ -1,24 +1,50 @@
 let fields = [];
+let gameOver = false;
 
 let currentShape = 'cross';
+let winner;
+
+/**
+    *Highlight the current player
+*/
 
 function fillShape(id) {
 
-    if (!fields[id]) {
+    if (!fields[id] && !gameOver) {
         if (currentShape == 'cross') {
-            currentShape = 'circle';
-            document.getElementById('player1').classList.remove('player-inactive');
-            document.getElementById('player2').classList.add('player-inactive');
+            currentShapeCross();
         } else {
-            currentShape = 'cross';
-            document.getElementById('player1').classList.add('player-inactive');
-            document.getElementById('player2').classList.remove('player-inactive');
+            currentShapeCircle();
         }
         fields[id] = currentShape;
         draw();
         checkForWin();
     }
 }
+
+/**
+    *if current shape is cross
+*/
+
+function currentShapeCross() {
+    currentShape = 'circle';
+    document.getElementById('player1').classList.remove('player-inactive');
+    document.getElementById('player2').classList.add('player-inactive');
+}
+
+/**
+    *if current shape is circle
+*/
+
+function currentShapeCircle() {
+    currentShape = 'cross';
+    document.getElementById('player1').classList.add('player-inactive');
+    document.getElementById('player2').classList.remove('player-inactive');
+}
+
+/**
+    *draw gamefield
+*/
 
 function draw() {
     for (let i = 0; i < fields.length; i++) {
@@ -35,59 +61,119 @@ function draw() {
     }
 }
 
+/**
+    *check whether someone has won
+*/
+
 function checkForWin() {
-    conditionsForWin();
+    fromLeftToRightWin();
+    fromAboveToBottomWin();
+    diagonalWin();
 }
 
-function conditionsForWin() {
-    let winner;
-    //die sind von links nach rechts
+/**
+    *check whether someone has won from left to right
+*/
+
+function fromLeftToRightWin() {
     if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0];
         document.getElementById('line-0').style.transform = 'scaleX(1)';
+        updateWinner(winner);
     }
 
     if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
         winner = fields[3];
         document.getElementById('line-1').style.transform = 'scaleX(1)';
+        updateWinner(winner);
     }
 
     if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
         winner = fields[6];
         document.getElementById('line-2').style.transform = 'scaleX(1)';
+        updateWinner(winner);
     }
+}
 
-    //die sind von oben nach unten
+/**
+    *check whether someone has won from above to bottom
+*/
+
+function fromAboveToBottomWin() {
     if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
         winner = fields[1];
         document.getElementById('line-3').style.transform = 'rotate(90deg) scaleX(1)';
+        updateWinner(winner);
     }
-    
+
     if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         winner = fields[2];
         document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1)';
-        
+        updateWinner(winner);
     }
 
     if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0];
         document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1)';
+        updateWinner(winner);
     }
+}
 
-    //die beiden mit Diagonal gewinnt
+/**
+    *check whether someone has won diagonal
+*/
+
+function diagonalWin() {
     if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0];
         document.getElementById('line-6').style.transform = 'rotate(45deg) scaleX(1)';
+        updateWinner(winner);
     }
 
     if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
         winner = fields[2];
         document.getElementById('line-7').style.transform = 'rotate(-45deg) scaleX(1)';
+        updateWinner(winner);
     }
+}
 
+/**
+    *if some has won
+*/
+
+function updateWinner(winner) {
     if (winner) {
         console.log('GEWONNEN', winner);
+        gameOver = true;
+        setTimeout(function () {
+            document.getElementById('game-over').classList.remove('d-none');
+            document.getElementById('restart-btn').classList.remove('d-none');
+        }, 1000);
     }
+}
+
+/**
+    *restart the game
+*/
+
+function restartGame() {
+    document.getElementById('game-over').classList.add('d-none');
+    document.getElementById('restart-btn').classList.add('d-none');
+    document.getElementById('line-0').style.transform = 'scaleX(0.0)';
+    document.getElementById('line-1').style.transform = 'scaleX(0.0)';
+    document.getElementById('line-2').style.transform = 'scaleX(0.0)';
+    document.getElementById('line-3').style.transform = 'rotate(90deg) scaleX(0.0)';
+    document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(0.0)';
+    document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(0.0)';
+    document.getElementById('line-6').style.transform = 'rotate(45deg) scaleX(0.0)';
+    document.getElementById('line-7').style.transform = 'rotate(-45deg) scaleX(0.0)';
+
+    for (let i = 0; i < fields.length; i++) {
+        document.getElementById('circle-' + i).classList.add('d-none');
+        document.getElementById('cross-' + i).classList.add('d-none');
+    }
+    fields = [];
+    gameOver = false;
 }
 
 /*
